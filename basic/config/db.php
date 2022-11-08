0157737c8a9eec07ce6a91dc24962e71b3161d1f
Saul-prog <sauloterogarcia54@gmail.com>
2022-11-08 19:52:05 +0100

    Conexión con base de datos

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=daw_tienda',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
